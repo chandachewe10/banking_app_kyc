c703b6e71647a5d37ca7122eabc9c71071e3f392
chandachewe10 <chewec03@gmail.com>
2025-06-14 16:16:56 +0200

    feat: added spalsh screens

---
+++ lib/constants/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFB4E3E6);
+const kRedColor = Color(0xFFD80310);
+const kGreenColor = Color(0xFF33C700);
+const kYellowColor = Color(0xFFF5B102);
+
+// *PRIMARY COLOR SHADES */
+const primaryColor50 = Color(0xFF87A9F3);
+const primaryColor100 = Color(0xFF598BF8);
+const primaryColor200 = Color(0xFF3A72EC);
+const primaryColor300 = Color(0xFF1D54CC);
+const primaryColor400 = Color(0xFF0239B0);
+const primaryColor500 = Color(0xFF0033A1);
+const primaryColor600 = Color(0xFF003099);
+const primaryColor700 = Color(0xFF002B88);
+const primaryColor800 = Color(0xFF042B80);
+const primaryColor900 = Color(0xFF002445);
+const primaryColor900_50 = Color(0xFF002445);
+
+// *NEUTRAL COLOR SHADES */
+const nuetralColor100 = Color(0xFFEEEEEE);
+const nuetralColor400 = Color(0xFFAFAFAF);
+const nuetralColor600 = Color(0xFF747474);
+const nuetralColor800 = Color(0xFF272727);
+const nuetralColor900 = Colors.black;
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:flutter_svg/svg.dart';
 void main() {
   runApp(const Homescreen());
 }
+
+
+
 
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
@@ -10,10 +14,80 @@ class Homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-appBar: AppBar(title: const Text('Demo Home'),centerTitle:true ,),
 
-      ),
+        debugShowCheckedModeBanner: false,
+        home:
+        IntroductionScreen(
+          
+          globalBackgroundColor: Colors.white,
+          allowImplicitScrolling: true,
+          autoScrollDuration: 3000,
+          infiniteAutoScroll: true,
+
+
+          pages: [
+           
+            PageViewModel(
+                title: "Simple Process",
+                body: "Qick and Simple",
+                image: Center(
+                  child: Image.asset("assets/images/introductory2.png"),
+                )),
+            PageViewModel(
+              title: "Welcome the Best Bank",
+              body: "Qick and Simple",
+              image: Center(
+                child: Image.asset("assets/images/introductory3.png"),
+              ),),
+            PageViewModel(
+                title: "Welcome the Best Bank",
+                body: "Qick and Simple",
+                image: Center(
+                  child: SvgPicture.asset("assets/images/introductory4.svg"),
+                )),
+
+          ],
+
+
+
+          onDone: () => {},
+          onSkip: () => {},
+          // You can override onSkip callback
+          showSkipButton: true,
+          skipOrBackFlex: 0,
+          nextFlex: 0,
+          showBackButton: false,
+          //rtl: true, // Display as right-to-left
+          back: const Icon(Icons.arrow_back),
+          skip: const Text(
+              'Skip', style: TextStyle(fontWeight: FontWeight.w600,color: Colors.white)),
+          next: const Icon(Icons.arrow_forward),
+          done: const Text(
+              'Done', style: TextStyle(fontWeight: FontWeight.w600,color: Colors.white)),
+          curve: Curves.fastLinearToSlowEaseIn,
+          controlsMargin: const EdgeInsets.all(16),
+
+          dotsDecorator: const DotsDecorator(
+            size: Size(10.0, 10.0),
+            color: Color(0xFFBDBDBD),
+            activeSize: Size(22.0, 10.0),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25.0)),
+            ),
+          ),
+          dotsContainerDecorator: const ShapeDecoration(
+            color: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
+          ),
+        )
+
+
+
     );
-  }
-}
+  }}
+
+
+
+
